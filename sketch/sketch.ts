8f43a0956983601d95733898708d2a3c3d9d9a9f
savila16 <91605655+savila16@users.noreply.github.com>
2021-10-15 09:48:56 -0300

    Add files via upload

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -32,9 +32,9 @@ class Board {
   draw (): void {
     image (this.background, 0, 0, this.nc * this.step, this.nl * this.step);
      for (let x = 0; x < this.nc; x ++){
-     for (let y = 0; y < this.nl; y ++){
+      for (let y = 0; y < this.nl; y ++){
          noFill ();
-         stroke (0);
+         stroke (100);
          strokeWeight (2);
          rect (x * this.step, y * this.step, this.step, this.step);
        }
@@ -42,13 +42,37 @@ class Board {
   }
 }
 
-let gato_img: p5.Image;
-let rato_img: p5.Image;
-let board_img: p5.Image;
-let game_over: boolean = false;
+class Obstaculo {
+  x: number;
+  y: number;
+  step: number;
+  image: p5.Image;
 
-let gato: Entity;
-let rato: Entity;
+  constructor (x: number, y: number, step: number, image: p5.Image){
+    this.x = x;
+    this.y = y;
+    this.step = step;
+    this.image = image; 
+  }
+
+  draw (): void {
+    image (this.image,  this.x * this.step, this.y * this.step, this.step, this.step);
+  }
+}
+
+let esquilo_img: p5.Image;
+let fogo_img: p5.Image;
+let noz_img: p5.Image;
+let board_img: p5.Image;
+let tempo: number; 
+let ganhou: boolean = false;
+
+let esquilo: Entity;
+let noz: Entity;
+let fogo_0: Obstaculo;
+let fogo_1: Obstaculo;
+let fogo_2: Obstaculo;
+let fogo_3: Obstaculo;
 let board: Board;
 
 function loadImg (path: string): p5.Image {
@@ -60,84 +84,125 @@ function loadImg (path: string): p5.Image {
 }
 
 function preload () {
-  gato_img = loadImg ('../sketch/gato.png');
-  rato_img = loadImg ('../sketch/rato.png');
-  board_img = loadImg ('../sketch/chão.jpg');
+  esquilo_img = loadImg ('../sketch/esquilo.png');
+  fogo_img = loadImg ('../sketch/fogo.png');
+  noz_img = loadImg ('../sketch/noz.png');
+  board_img = loadImg ('../sketch/gelo.png');
 }
 
 function keyPressed (){
 
-  if (game_over == false){
+  if (ganhou == false){
     if (keyCode === LEFT_ARROW){
-      gato.x--;
+      esquilo.x--;
     } else if (keyCode === RIGHT_ARROW){
-      gato.x++;
+      esquilo.x++;
     } else if (keyCode === UP_ARROW){
-      gato.y--;
+      esquilo.y--;
     } else if (keyCode === DOWN_ARROW){
-      gato.y++;
+      esquilo.y++;
     }
   
     if (keyCode === "A".charCodeAt (0)){
-      rato.x--;
+      noz.x--;
     } else if (keyCode === "D".charCodeAt (0)){
-      rato.x++;
+      noz.x++;
     } else if (keyCode === "W".charCodeAt (0)){
-      rato.y--;
+      noz.y--;
     } else if (keyCode === "S".charCodeAt (0)){
-      rato.y++;
+      noz.y++;
     }
   }
 }
 
 function limite (): any {
 
-  if (gato.x < 0){
-    gato.x ++
-  } else if (gato.x > board.nc -1){
-    gato.x--
-  } else if (gato.y < 0){
-    gato.y++
-  } else if (gato.y > board.nl - 1){
-    gato.y--
+  if (esquilo.x < 0){
+    esquilo.x ++
+  } else if (esquilo.x > board.nc -1){
+    esquilo.x--
+  } else if (esquilo.y < 0){
+    esquilo.y++
+  } else if (esquilo.y > board.nl - 1){
+    esquilo.y--
   }
 
-  if (rato.x < 0){
-    rato.x++
-  } else if (rato.x > board.nc - 1){
-    rato.x--
-  } else if (rato.y < 0){
-    rato.y++
-  } else if (rato.y > board.nl - 1){
-    rato.y--
+  if (noz.x < 0){
+    noz.x++
+  } else if (noz.x > board.nc - 1){
+    noz.x--
+  } else if (noz.y < 0){
+    noz.y++
+  } else if (noz.y > board.nl - 1){
+    noz.y--
   }
 }
 
-function colisao (): boolean {
-  if (gato.x == rato.x && gato.y == rato.y && gato.x == rato.y && gato.y == rato.x){
+function colisao (): void {
+  if (esquilo.x == noz.x && esquilo.y == noz.y){
+   frameRate (0);
    background (200);
+   fill (50);
    textSize (50);
    textAlign (CENTER, CENTER);
-   text ("GAME OVER", 200, 200);
-   game_over = true;
-   return true
+   text ("GANHOU!", 300, 250);
+   ganhou = true;
+  }
+  if (esquilo.x == fogo_0.x && esquilo.y == fogo_0.y) {
+    frameRate (0);
+    background (230);
+    fill (50);
+    text ("MORREU :(", 200, 250);
+  } if (esquilo.x == fogo_1.x && esquilo.y == fogo_1.y) {
+    frameRate (0);
+    fill (50);
+    background (230);
+    text ("MORREU :(", 200, 250);
+  } if (esquilo.x == fogo_2.x && esquilo.y == fogo_2.y) {
+    frameRate (0);
+    fill (50);
+    background (230);
+    text ("MORREU :(", 200, 250);
+  } if (esquilo.x == fogo_3.x && esquilo.y == fogo_3.y) {
+    frameRate (0);
+    fill (50);
+    background (230);
+    text ("MORREU :(", 200, 250);
+  } 
+}
+
+function timer (){
+  if (frameCount >= 30*60){
+  background (200);
+  fill (50);
+  text ("TEMPO ESGOTADO!", 100, 250);
   } else {
-    return false;
+  textSize (40);
+  text ((Math.floor((30*60 - frameCount) / 60)), 540, 70);
   }
 }
 
 function setup (){
   let size = 100;
-  gato = new Entity (2, 2, size, gato_img);
-  rato = new Entity (1, 1, size, rato_img);
-  board = new Board (4, 4, size, board_img);
+  esquilo = new Entity (5, 4, size, esquilo_img);
+  fogo_0 = new Obstaculo (1, 1, size, fogo_img);
+  fogo_1 = new Obstaculo (4, 1, size, fogo_img);
+  fogo_2 = new Obstaculo (1, 3, size, fogo_img);
+  fogo_3 = new Obstaculo (4, 3, size, fogo_img);
+  noz = new Entity (0, 0, size, noz_img);
+  board = new Board (6, 5, size, board_img);
   createCanvas (board.nc * size, board.nl * size);
 }
 
 function draw (){
   limite ();
   board.draw ();
-  gato.draw();
-  rato.draw ();
+  esquilo.draw();
+  noz.draw ();
+  fogo_0.draw ();
+  fogo_1.draw ();
+  fogo_2.draw ();
+  fogo_3.draw ();
   colisao ();
+  timer ();
 }
